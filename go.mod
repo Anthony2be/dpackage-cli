ed4825e2a6d41f025a8bb00424712ca6a93822ef
Anthony2be <adubovitsky1234@gmail.com>
2022-04-16 14:55:07 -0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/anthony2be/dpackage
+
+go 1.16
+
+require github.com/spf13/cobra v1.4.0
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 Anthony Dubovitsky adubovitsky1234@gmail.com
+
+*/
+package main
+
+import "github.com/anthony2be/dpackage/cmd"
+
+func main() {
+	cmd.Execute()
+}
