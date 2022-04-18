270c68b76818490438da5d3226022e296433dfa9
Anthony2be <adubovitsky1234@gmail.com>
2022-04-17 22:14:44 -0700

    work on it a bit

--- cmd/install.go
+++ cmd/install.go
@@ -1,27 +1,72 @@
 /*
-Copyright © 2022 Anthony Dubovitsky adubovitsky1234@gmail.com
+Copyright © 2022 Anthony Dubovitsky
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 package cmd
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
 
+	"github.com/dustin/go-humanize"
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
+// WriteCounter counts the number of bytes written to it. It implements to the io.Writer interface
+// and we can pass this into io.TeeReader() which will report progress on each write cycle.
+type WriteCounter struct {
+	Total uint64
+}
+
+func (wc *WriteCounter) Write(p []byte) (int, error) {
+	n := len(p)
+	wc.Total += uint64(n)
+	wc.PrintProgress()
+	return n, nil
+}
+
+func (wc WriteCounter) PrintProgress() {
+	// Clear the line by using a character return to go back to the start and remove
+	// the remaining characters by filling it with spaces
+	fmt.Printf("\r%s", strings.Repeat(" ", 35))
+
+	// Return again and print current status of download
+	// We use the humanize package to print the bytes in a meaningful way (e.g. 10 MB)
+	fmt.Printf("\rDownloading... %s complete", humanize.Bytes(wc.Total))
+}
+
+var World string
+
 // installCmd represents the install command
 var installCmd = &cobra.Command{
-	Use:   "install",
+	Use:   "install <datapack name>",
 	Short: "Install a datapack",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `Install a datapack from the Dpackage website`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("install called")
+		fmt.Println("Download Started")
+
+		fileUrl := "https://github.com/MMK21Hub/Capitalisation-Fixes/releases/download/v2.4/Capitalisation-Fixes-v2.4.zip//args[0]" //"https://upload.wikimedia.org/wikipedia/commons/d/d6/Wp-w4-big.jpg"
+		err := DownloadFile(fmt.Sprintf("%s.zip", args[0]), fileUrl)
+		if err != nil {
+			panic(err)
+		}
+
+		color.Green("Download Finished!")
 	},
 }
 
@@ -37,4 +82,45 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// installCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	//installCmd.PersistentFlags().String("test", "", "test")
+	installCmd.PersistentFlags().StringVarP(&World, "world", "w", "", "World name")
+}
+
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory. We pass an io.TeeReader
+// into Copy() to report progress on the download.
+func DownloadFile(filepath string, url string) error {
+
+	// Create the file, but give it a tmp file extension, this means we won't overwrite a
+	// file until it's downloaded, but we'll remove the tmp extension once downloaded.
+	out, err := os.Create(filepath + ".tmp")
+	if err != nil {
+		return err
+	}
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		out.Close()
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create our progress reporter and pass it to be used alongside our writer
+	counter := &WriteCounter{}
+	if _, err = io.Copy(out, io.TeeReader(resp.Body, counter)); err != nil {
+		out.Close()
+		return err
+	}
+
+	// The progress use the same line so print a new line once it's finished downloading
+	fmt.Print("\n")
+
+	// Close the file without defer so it can happen before Rename()
+	out.Close()
+
+	if err = os.Rename(filepath+".tmp", filepath); err != nil {
+		return err
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/anthony2be/dpackage
 
 go 1.16
 
-require github.com/spf13/cobra v1.4.0
+require (
+	github.com/dustin/go-humanize v1.0.0
+	github.com/fatih/color v1.13.0
+	github.com/spf13/cobra v1.4.0
+)
